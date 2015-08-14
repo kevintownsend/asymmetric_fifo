bda833bfa574432a509e4de0872e02d4f712e9d2
kevintownsend <kt@krtownsend.com>
2015-08-14 16:09:50 -0500

    .

--- makefile
+++ makefile
@@ -1,3 +1,6 @@
+vim :
+	vim -p asymmetric_fifo.v asymmetric_fifo_tb.v ../ram/asymmetric_distributed_ram.v makefile vsim.mk
+
 sim :
 	iverilog -o std_fifo_tb.vvp -I../common std_fifo_tb.v std_fifo.v
 	std_fifo_tb.vvp
--- vsim.mk
+++ vsim.mk
@@ -1,19 +1,24 @@
 
-vsim_std_fifo = work/std_fifo
+vsim_asymmetric_fifo = work/asymmetric_fifo
 
-vsim_std_fifo_tb = work/std_fifo_tb
+vsim_asymmetric_fifo_tb = work/asymmetric_fifo_tb
+
+vsim_asymmetric_distributed_ram = work/asymmetric_distributed_ram
 
 work :
 	vlib work
 
-$(vsim_std_fifo) : std_fifo.v work
-	vlog std_fifo.v +incdir+../common
+$(vsim_asymmetric_fifo) : asymmetric_fifo.v work
+	vlog asymmetric_fifo.v +incdir+../common
 
-$(vsim_std_fifo_tb) : std_fifo_tb.v work
-	vlog std_fifo_tb.v +incdir+../common
+$(vsim_asymmetric_fifo_tb) : asymmetric_fifo_tb.v work
+	vlog asymmetric_fifo_tb.v +incdir+../common
+
+$(vsim_asymmetric_distributed_ram) : ../ram/asymmetric_distributed_ram.v work
+	vlog ../ram/asymmetric_distributed_ram.v +incdir+../common
 
 cleanVsim :
 	rm -rf work transcript
 
-checkVsim : work $(vsim_std_fifo_tb) $(vsim_std_fifo)
-	echo -e "vsim work.std_fifo_tb\nrun -all" | vsim
+checkVsim : work $(vsim_asymmetric_fifo_tb) $(vsim_asymmetric_fifo) $(vsim_asymmetric_distributed_ram)
+	echo -e "vsim work.asymmetric_fifo_tb\nrun -all" | vsim
